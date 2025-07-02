b515007593017d9c5b390320599328b9aad3c88f
Sandhiya G <sandhiyagy@gmail.com>
2025-07-02 22:16:04 +0530

    Create maximum-subarray.java

---
+++ skillrack-july-2025/maximum-subarray.java
@@ -0,0 +1,16 @@
+//Approach:Kadane's Algorithm
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int LEN=nums.length;
+        int larSum=nums[0];
+        int curSum=0;
+        for(int i=0;i<LEN;i++){
+            curSum+=nums[i];
+            larSum=(curSum>larSum)?curSum:larSum;
+            if(curSum<0){
+                curSum=0;
+            }
+        }
+        return larSum;
+    }
+}
