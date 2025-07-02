9dfbdee7a119ab9e9048af1911c2aa887a180da8
Sandhiya G <sandhiyagy@gmail.com>
2025-07-02 23:05:20 +0530

    Update longest-increasing-subsequence.java

--- skillrack-july-2025/longest-increasing-subsequence.java
+++ skillrack-july-2025/longest-increasing-subsequence.java
@@ -1,6 +1,6 @@
 // LEETCODE:300
   
-// Approach:Binary Search
+// Approach 1:Binary Search [TC:O(n log n)]
 // Create a list
 // Every iteration has two type of actions
 //   ->If the list is empty(since arr[0] does not have greater element) ||the last element is lesser than the current element appends it 
@@ -49,6 +49,61 @@ class Solution {
     }
 }
 
+
+// Approach2:DP [TC:O(n^2)]
   
+// Example:
+// arr=10 9 2 5 3 7 101 18
+// lis=1  1 1 1 1 1 1   1
+
+// Row=0:
+// arr=10 9 2 5 3 7 101 18
+// lis=1  1 1 1 1 1 1   1
+  
+// Row=1:
+// arr=10 9 2 5 3 7 101 18
+// lis=1  1 1 1 1 1 1   1
+  
+// Row=2:
+// arr=10 9 2 5 3 7 101 18
+// lis=1  1 1 1 1 1 1   1
+  
+// Row=3:
+// arr=10 9 2 5 3 7 101 18
+// lis=1  1 1 2 1 1 1   1
+  
+// Row=4:
+// arr=10 9 2 5 3 7 101 18
+// lis=1  1 1 2 2 1 1   1
+  
+// Row=5:
+// arr=10 9 2 5 3 7 101 18
+// lis=1  1 1 2 2 3 1   1
+  
+// Row=6:
+// arr=10 9 2 5 3 7 101 18
+// lis=1  1 1 2 2 3 4   1
+  
+// Row=7:
+// arr=10 9 2 5 3 7 101 18
+// lis=1  1 1 2 2 3 4   4
+// Final lis:1 1 1 2 2 3 4 4 => max=4
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int maxLen=1;
+        int len=nums.length;
+        int[] lis=new int[len];
+        Arrays.fill(lis, 1); 
+        for(int row=0;row<len;row++){
+            for(int col=0;col<row;col++){
+                if(nums[row]>nums[col]){
+                    lis[row]=Math.max(lis[row],lis[col]+1);
+                    maxLen=Math.max(maxLen,lis[row]);
+                }
+            }
+        }
+        return maxLen;
+    }
+}
   
   
